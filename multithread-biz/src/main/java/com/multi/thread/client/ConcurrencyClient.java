1be6f38d66baec488b5700d02cff68aa3af4783c
hongmingxing <12615041@qq.com>
2019-05-16 19:10:48 +0800

    【提交】     1、多线程/单线程效率对比     2、死锁示例

---
+++ multithread-biz/src/main/java/com/multi/thread/client/ConcurrencyClient.java
@@ -0,0 +1,51 @@
+package com.multi.thread.client;
+
+/**
+ * @description:
+ * @name: ConcurrencyClient
+ * @author: Stars Hung
+ * @date: 16:49 2019/5/16
+ **/
+public class ConcurrencyClient {
+    /**
+     * 数据量达到1亿时，多线程效率才超过串行操作
+     */
+    private static final Long COUNT = 100000000L;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    private static void concurrency() throws InterruptedException {
+        Long start = System.currentTimeMillis();
+        Thread thread = new Thread(() -> {
+            int a = 0;
+            for (long i = 0; i < COUNT; i++) {
+                a += 5;
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < COUNT; i++) {
+            b--;
+        }
+        Long time = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("concurrency :" + time + "ms, b=" + b);
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < COUNT; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (int i = 0; i < COUNT; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial: " + time + "ms,b=" + b + ",a = " + a);
+    }
+}
---
+++ multithread-biz/src/main/java/com/multi/thread/client/DeadLockDemo.java
@@ -0,0 +1,40 @@
+package com.multi.thread.client;
+
+/**
+ * @description:
+ * @name: DeadLockDemo
+ * @author: Stars Hung
+ * @date: 17:52 2019/5/16
+ **/
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread t1 = new Thread(() -> {
+            synchronized (A) {
+                try {
+                    Thread.currentThread().sleep(2000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                synchronized (B) {
+                    System.out.println("1");
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            synchronized (B) {
+                synchronized (A) {
+                    System.out.println("2");
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
